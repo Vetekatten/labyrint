a53c8e61040615021af93b10ca41e049d2fd43f9
Joen Wilund <vetekatten3@gmail.com>
2021-12-15 12:36:35 +0100

    asdfghjklöä

---
+++ labyrint/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+int room;
+room=1;
+
+
+while (room == 1){
+Console.WriteLine("Du står it ett rum av sten, det finns en dörr i det. Går du vidare?");
+if (Console.ReadLine() == "ja"){
+    room = 2;
+}
+else if (Console.ReadLine() == "nej"){
+    room = 99;
+}
+else if ((Console.ReadLine() != "ja") || (Console.ReadLine() != "nej")) {
+    Console.WriteLine("Fel skrivet, skriv ja eller nej");
+}
+}
+
+while (room == 2){
+Console.WriteLine("Du kommer in i ett andra rum gjort av trä, vill du gå vidare?");
+if (Console.ReadLine() == "ja"){
+    room = 3;
+}
+else if (Console.ReadLine() == "nej"){
+    room = 1;
+}
+else {
+    Console.WriteLine("Fel skrivet, skriv ja eller nej");
+}
+}
+
+while (room == 3){
+Console.WriteLine("Du kommer in i ett tredje rum av metall, vill du gå ut?");
+if (Console.ReadLine() == "ja"){
+    room = 4;
+}
+else if (Console.ReadLine() == "nej"){
+    room = 2;
+}
+else {
+    Console.WriteLine("Fel skrivet, skriv ja eller nej");
+}
+}
+if (room == 4){
+    Console.WriteLine("Du kommer ut ur byggnaden, hejdå");
+    Console.ReadLine();
+}
+while (room == 99){
+    Console.WriteLine("Du står still i rummet tills du dör eftersom du inte har något vatten");
+    Console.ReadLine();
+}
